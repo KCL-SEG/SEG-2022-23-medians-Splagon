61ba121c87f2d5ac117c5cd86c241a1251e8300b
Splagon <misha.higham@hotmail.co.uk>
2022-09-30 16:15:50 +0100

    Implement list sorting and median calculation

--- median.py
+++ median.py
@@ -9,4 +9,19 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort();
+
+median = 0;
+
+if (len(numbers) > 0):
+    midpoint = (len(numbers) + 1) / 2 - 1;
+    if (len(numbers) % 2 == 1): #odd
+        median = numbers[int(midpoint)]
+    else:
+        lowerOfMedian = numbers[int(midpoint - 0.5)]
+        upperOfMedian = numbers[int(midpoint + 0.5)]
+        median = (lowerOfMedian + upperOfMedian) / 2
+
+print()
+print("Median = " + str(median))
